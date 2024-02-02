cfe70eb28125837c31a49e97089282cdefb9933f
Cristian B <cris.bahena77@gmail.com>
2024-02-02 15:27:15 -0600

    Added sign up, sessions route and test, login, and user authentication

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,4 +1,6 @@
 class UsersController < ApplicationController
+  before_action :authenticate_user, except: [:index, :show, :create]
+
   def index
     @users = User.all
     render :index
@@ -26,19 +28,27 @@ class UsersController < ApplicationController
 
   def update
     @user = User.find_by(id: params[:id])
-    @user.update(
+    if current_user.id == @user.id
+      @user.update(
       name: params[:name] || @user.name,
       email: params[:email] || @user.email,
       password: params[:password] || @user.password,
       password_confirmation: params[:password_confirmation] || @user.password_confirmation,
       image_url: params[:image_url] || @user.image_url
     )
-    render :show
+      render :show
+    else
+      render json:{message: "Login with correct account."}
+    end
   end
 
   def destroy
     @user = User.find_by(id: params[:id])
-    @user.destroy
-    render json:{message: "User deleted succesfully"}
+    if current_user.id == @user.id
+      @user.destroy
+      render json:{message: "User deleted succesfully"}
+    else
+      render json:{message: "Login with correct account."}
+    end
   end
 end
