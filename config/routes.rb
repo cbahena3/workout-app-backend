2d9aff5854e9185b606210bf2e2ea1bb31e6dfab
Cristian B <cris.bahena77@gmail.com>
2024-02-02 15:18:16 -0600

    Added sign up, sessions route and test, login, and user authentication

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,11 @@
 Rails.application.routes.draw do
+  #user routes
   get "/users" => "users#index"
   get "/users/:id" => "users#show"
   post "/users" => "users#create"
   patch "users/:id" => "users#update"
   delete "users/:id" => "users#destroy"
+
+  #sessions routes
+  post "/sessions" => "sessions#create"
 end
