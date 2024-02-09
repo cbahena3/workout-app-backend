b27d36fb543c1a6837bd88275b6f9745293ee3ee
Cristian B <cris.bahena77@gmail.com>
2024-02-08 22:21:13 -0600

    added authentication to exercise routine updates so only the creator can change the info on the exercise routine

--- app/controllers/exercise_routines_controller.rb
+++ app/controllers/exercise_routines_controller.rb
@@ -16,7 +16,6 @@ class ExerciseRoutinesController < ApplicationController
     exercise = Exercise.find_by(name: params[:exercise])
     params[:routine]
     routine = Routine.find_by(name: params[:routine])
-
     @exercise_routine = ExerciseRoutine.create(
       exercise_id: exercise.id,
       routine_id: routine.id,
@@ -32,17 +31,17 @@ class ExerciseRoutinesController < ApplicationController
 
   def update
     @exercise_routine = ExerciseRoutine.find_by(id: params[:id])
-    # if current_user.id == @exercise_routine.id
-    @exercise_routine.update(
-      exercise_id: params[:exercise_id] || @exercise_routine.exercise_id,
-      routine_id: params[:routine_id] || @exercise_routine.routine_id,
-      sets: params[:sets] || @exercise_routine.sets,
-      reps: params[:reps] || @exercise_routine.reps
-    )
-    render :show
-    # else
-    # render json:{message: "Login with correct account."}
-    # end
+    if current_user.id == @exercise_routine.routine.user_id
+      @exercise_routine.update(
+        exercise_id: params[:exercise_id] || @exercise_routine.exercise_id,
+        routine_id: params[:routine_id] || @exercise_routine.routine_id,
+        sets: params[:sets] || @exercise_routine.sets,
+        reps: params[:reps] || @exercise_routine.reps
+      )
+      render :show
+    else
+      render json:{message: "Login with correct account."}
+    end
   end
 
   def destroy
--- app/views/exercise_routines/_exercise_routine.json.jbuilder
+++ app/views/exercise_routines/_exercise_routine.json.jbuilder
@@ -3,3 +3,4 @@ json.exercise_id exercise_routine.exercise_id
 json.routine_id exercise_routine.routine_id
 json.sets exercise_routine.sets
 json.reps exercise_routine.reps
+json.routine exercise_routine.routine.user_id
